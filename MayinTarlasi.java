da9f694ea75e0251af3fe197be3970c4f6fec5cf
sercel123 <ercelserap@gmail.com>
2020-03-19 22:04:26 +0300

    Update MayinTarlasi.java

--- MayinTarlasi.java
+++ MayinTarlasi.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class MayinTarlasi {
 	public static void main(String args[]) {
 		Scanner s=new Scanner(System.in);
-		System.out.println("Oynamak istediğin tarlanın en ve boy değerlerini sırasıyla gir:");
+		System.out.println("Oynamak istediÄŸin tarlanÄ±n en ve boy deÄŸerlerini sÄ±rasÄ±yla gir:");
 		int en=s.nextInt();
 		int boy=s.nextInt();
 		int [][] matris =new int [en][boy];
@@ -16,7 +16,7 @@ public class MayinTarlasi {
 				matris[i][j]=sayi;
 			}
 		}
-		System.out.println("İşte tarlanın haritası dikkatli ol ve en yuksek puanı topla!");
+		System.out.println("Ä°ÅŸte tarlanÄ±n haritasÄ± dikkatli ol ve en yuksek puanÄ± topla!");
 		for(int i=0;i<en;i++) {
 			for(int j=0;j<boy;j++) {
 				System.out.print(" "+i+""+j);
@@ -24,19 +24,19 @@ public class MayinTarlasi {
 		}
 		do {
 			try {
-				System.out.println("Seçtiğin adresin kordinatlarını gir:");
+				System.out.println("SeÃ§tiÄŸin adresin kordinatlarÄ±nÄ± gir:");
 				int kordinatx=s.nextInt();
 				int kordinaty=s.nextInt();
 				if(matris[kordinatx][kordinaty]==1) {
-					System.out.println("Mayına bastın :( Puanın: "+puan);
+					System.out.println("MayÄ±na bastÄ±n :( PuanÄ±n: "+puan);
 					break;
 				}
 				else {
-					System.out.println("Harika hayattasın hadi devam edelim..");
+					System.out.println("Harika hayattasÄ±n hadi devam edelim..");
 					puan=puan+10;
 				}
 			}catch(Exception e) {
-				System.out.println("kordinatların tarla içinen olmalı :) yeniden dene\n");
+				System.out.println("kordinatlarÄ±n tarla iÃ§inden olmalÄ± :) yeniden dene");
 			}
 		}while(true);
 	}
